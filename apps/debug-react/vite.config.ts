469191dc294df97911bc47e535a706258629b0e7
马逸𫍽 <mayixuan1@yiche.com>
2025-02-16 23:21:20 +0800

    feat: 引入react18.3.1带有map的umd包

---
+++ apps/debug-react/vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { viteExternalsPlugin } from "vite-plugin-externals";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react({
+      jsxRuntime: "classic",
+    }),
+    viteExternalsPlugin(
+      {
+        react: "React", // 映射react的全局变量为React
+        "react-dom": "ReactDOM",
+        "react-dom/client": "ReactDOM",
+      },
+      // {
+      //   disableInServe: true,
+      // }
+    ),
+  ],
+});
